81e16157d7a87bb09889d1c8eef0448c6c93ba04
Jimmi James <133916219+jimmijameslinux@users.noreply.github.com>
2024-12-29 11:59:21 +0000

    some more ui changes

--- src/components/Stories.jsx
+++ src/components/Stories.jsx
@@ -32,11 +32,50 @@ function Stories() {
                 <span>Skin Specialist</span>
               </div>
             </div>
-            {/* <h2>स्वस्थस्य स्वास्थ्य रक्षणं, आतुरस्य विकार प्रशमनं ।"</h2> */}
-            {/* <p>
-              [ Meaning: The Goal of Ayurveda is to maintain the health of a
-              healthy person and to cure the disease of a diseased person. ]
-            </p> */}
+          </div>
+          <button>Book a session</button>
+        </div>
+        <div className="substories">
+          <div className="child">
+            <div className="imgcontainer">
+              <img src={expertone} alt="" />
+              <img src={expertonerate} alt="" />
+            </div>
+
+            <div className="storycontent">
+              <h3>Dr. Vaishali sharma</h3>
+              <span>Ayurveda Practitioner (BAMS, MD)</span>
+              <div className="degree">
+                <img className="degreei" src={degree} alt="error" />
+                <span>25 years of experience</span>
+              </div>
+              <div className="pin">
+                <img className="pini" src={pin} alt="error" />
+                <span>Skin Specialist</span>
+              </div>
+            </div>
+          </div>
+          <button>Book a session</button>
+        </div>
+        <div className="substories">
+          <div className="child">
+            <div className="imgcontainer">
+              <img src={expertone} alt="" />
+              <img src={expertonerate} alt="" />
+            </div>
+
+            <div className="storycontent">
+              <h3>Dr. Vaishali sharma</h3>
+              <span>Ayurveda Practitioner (BAMS, MD)</span>
+              <div className="degree">
+                <img className="degreei" src={degree} alt="error" />
+                <span>25 years of experience</span>
+              </div>
+              <div className="pin">
+                <img className="pini" src={pin} alt="error" />
+                <span>Skin Specialist</span>
+              </div>
+            </div>
           </div>
           <button>Book a session</button>
         </div>
